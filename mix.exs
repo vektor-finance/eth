ff91d598c96a42ae5d78217e1737e04a0f134933
Akram Hussein <akram@vektor.finance>
2023-03-22 11:02:01 -0300

    build: update deps

--- mix.exs
+++ mix.exs
@@ -27,15 +27,15 @@ defmodule Eth.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps() do
     [
-      {:ethereumex, "~> 0.7.0"},
+      {:ethereumex, "~> 0.10.4"},
       {:ex_rlp, "~> 0.5.3"},
       {:ex_doc, ">= 0.25.2", only: :dev},
       {:dialyxir, "~> 1.1.0", only: [:dev], runtime: false},
       {:hexate, "~> 0.6.1"},
-      {:ex_keccak, "~> 0.2.0"},
+      {:ex_keccak, "~> 0.7.1"},
       {:mnemonic, "~> 0.3.0"},
       {:poison, "~> 5.0.0", only: :test},
-      {:ex_secp256k1, "~> 0.2.1"},
+      {:ex_secp256k1, "~> 0.6.0"},
     ]
   end
 
